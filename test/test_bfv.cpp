3887cc2dc707bda70087511ab5525e5d23b10d19
velenchan <jingwei.chen@outlook.com>
2023-07-12 10:52:49 +0800

    update test bgv/bfv

--- test/test_bfv.cpp
+++ test/test_bfv.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main(){
 
+//     EncryptionParameters parms(scheme_type::bgv);
     EncryptionParameters parms(scheme_type::bfv);
     size_t poly_modulus_degree = 4096;
     parms.set_poly_modulus_degree(poly_modulus_degree);
@@ -14,8 +15,9 @@ int main(){
     We can certainly use BFVDefault coeff_modulus. In later parts of this example,
     we will demonstrate how to choose coeff_modulus that is more useful in BGV.
     */
-    parms.set_coeff_modulus(CoeffModulus::BFVDefault(poly_modulus_degree));
-    parms.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, 25));
+//     parms.set_coeff_modulus(CoeffModulus::BFVDefault(poly_modulus_degree));
+    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, {21, 22, 22, 22, 22}));
+    parms.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, 24));
 
 
     SEALContext context(parms);
@@ -56,10 +58,14 @@ int main(){
 
     vector<int64_t> x(slot_count, 0);
     // x[0] = -16769024;
-    x[0] = -14;
-    x[1] = 26;
-    x[2] = -3;
-    x[3] = 4;
+    x[0] = -1;
+    x[1] = 1;
+    x[2] = -1;
+    x[3] = 1;
+    x[slot_count-1] = 5;
+    x[slot_count-2] = 7;
+    x[slot_count-3] = 11;
+    x[slot_count-4] = 13;
 
     cout << "Input plaintext matrix:" << endl;
     print_vector(x);
@@ -78,6 +84,32 @@ int main(){
     cout << "    + noise budget in freshly encrypted x: " << decryptor.invariant_noise_budget(x_encrypted) << " bits"
          << endl;
 
+     Plaintext x_decrypted;
+     vector<int64_t> x_result;
+     cout << "    + decryption of x_encrypted: ";
+     decryptor.decrypt(x_encrypted, x_decrypted);
+     batch_encoder.decode(x_decrypted, x_result);
+     print_vector(x_result);
+     cout << "" << " ...... Correct." << endl;
+
+     Plaintext plain_two_thousand;
+     vector<int64_t> two_thousand;
+     for (size_t i = 0; i < slot_count; i++){
+          two_thousand.push_back(2);
+     }
+     batch_encoder.encode(two_thousand, plain_two_thousand);
+     evaluator.multiply_plain_inplace(x_encrypted, plain_two_thousand);
+
+     cout << "    + noise budget in encrypted x after multiplying 2000: " << decryptor.invariant_noise_budget(x_encrypted) << " bits"
+         << endl;
+
+
+     cout << "    + decryption of x_encrypted after multiplying 2000: ";
+     decryptor.decrypt(x_encrypted, x_decrypted);
+     batch_encoder.decode(x_decrypted, x_result);
+     print_vector(x_result);
+     cout << "" << " ...... Correct." << endl;
+
 
     Ciphertext x_square_encrypted;
     evaluator.square(x_encrypted, x_square_encrypted);
@@ -93,22 +125,29 @@ int main(){
     cout << "    + noise budget in encrypted x^2 after relinearization: " << decryptor.invariant_noise_budget(x_square_encrypted) << " bits"
          << endl;
 
-    evaluator.rotate_rows_inplace(x_square_encrypted, 3, galois_keys);
 
-    cout << "    + size of encrypted x^2 after rotation: " << x_square_encrypted.size() << endl;
+     cout << "    + decryption of x_square_encrypted after relinearization: ";
+     decryptor.decrypt(x_square_encrypted, x_decrypted);
+     batch_encoder.decode(x_decrypted, x_result);
+     print_vector(x_result);
+     cout << "" << " ...... Correct." << endl;
 
-    cout << "    + noise budget in encrypted x^2 after rotation: " << decryptor.invariant_noise_budget(x_square_encrypted) << " bits"
+
+     
+     evaluator.rotate_columns_inplace(x_square_encrypted, galois_keys);
+     evaluator.rotate_rows_inplace(x_square_encrypted, -4, galois_keys);
+
+     cout << "    + size of encrypted x^2 after rotation: " << x_square_encrypted.size() << endl;
+
+     cout << "    + noise budget in encrypted x^2 after rotation: " << decryptor.invariant_noise_budget(x_square_encrypted) << " bits"
          << endl;
 
 
     
-    Plaintext x_decrypted;
-    cout << "    + decryption of x_encrypted: ";
+    
+    cout << "    + decryption of x_encrypted after rotations: ";
     decryptor.decrypt(x_square_encrypted, x_decrypted);
-
-    vector<int64_t> x_result;
     batch_encoder.decode(x_decrypted, x_result);
-
     print_vector(x_result);
     cout << "" << " ...... Correct." << endl;
 
