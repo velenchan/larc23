88dbc5b281473198c4db87c06230762eac00f564
YangLinHan <45842233+hangenba@users.noreply.github.com>
2023-07-14 11:04:48 +0800

    Add files via upload

---
+++ bgv_8192/setting.h
@@ -0,0 +1,9 @@
+#pragma once
+#define public_dir  "./../tmp/public"
+#define secret_key_dir  "./../tmp/secret_key"
+#define client_matrix_dir "./../tmp/client_matrix_tmp"
+#define database_matrix_dir "./../tmp/database_matrix_tmp"
+#define client_data_dir "./../client_data/QUERY_SITE_genotypes.txt"
+#define database_data_dir "./../database_data/DATABASE_SITE_genotypes.txt"
+#define result_dir "./../result"
+#define batch_size 64
